bb5ec77cb059967f295416615b609b20372e72c2
wb20 <hello@wb20.cn>
2024-03-18 15:46:51 +0800

    first submit

--- r-rpc-easy/src/main/java/com/wb20/rrpc/proxy/ServiceProxy.java
+++ r-rpc-easy/src/main/java/com/wb20/rrpc/proxy/ServiceProxy.java
@@ -21,6 +21,11 @@ public class ServiceProxy implements InvocationHandler {
      * @return
      * @throws Throwable
      */
+    //在 Java 的动态代理中，这三个参数是由 Java 运行时系统自动传递的。
+    //proxy 参数表示代理对象本身，即通过动态代理生成的代理对象。在 invoke 方法中，通常不需要直接使用这个参数。
+    //method 参数表示被调用的方法对象，即客户端代码调用代理对象的哪个方法。在 invoke 方法中，通过这个参数可以获取到被调用的方法的信息，如方法名、参数类型等。
+    //args 参数表示方法调用时传入的参数数组，即客户端代码调用代理对象方法时传入的参数。在 invoke 方法中，通过这个参数可以获取到方法调用时传入的参数值。
+    //这三个参数是由 Java 动态代理机制在调用代理对象的方法时自动传递给 invoke 方法的，开发者无需手动传入。在 invoke 方法中，可以根据这些参数来动态地处理方法调用，例如构造远程调用请求、发送请求、处理响应等。
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
