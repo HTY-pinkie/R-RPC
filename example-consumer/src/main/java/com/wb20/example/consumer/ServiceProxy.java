ba5000210f7b33df301579ee3856c03120dfb3e4
wb20 <hello@wb20.cn>
2024-03-18 11:13:24 +0800

    first submit

--- example-consumer/src/main/java/com/wb20/example/consumer/ServiceProxy.java
+++ example-consumer/src/main/java/com/wb20/example/consumer/ServiceProxy.java
@@ -1,11 +1,56 @@
 package com.wb20.example.consumer;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.wb20.rrpc.model.RpcRequest;
+import com.wb20.rrpc.model.RpcResponse;
+import com.wb20.rrpc.serializer.JdkSerializer;
+import com.wb20.rrpc.serializer.Serializer;
+
+import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * 服务代理（JDK 动态代理）
+ */
 public class ServiceProxy implements InvocationHandler {
+    /**
+     * 调用代理
+     *
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        //指定序列化器
+        Serializer serializer = new JdkSerializer();
+
+        //构造请求
+        //动态代理依赖method里的信息，静态代理直接写死了这里
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args)
+                .build();
+        try {
+            //序列化请求
+            byte[] bodyBytes = serializer.serialize(rpcRequest);
+            //发送请求
+            // todo 注意，这里地址被硬编码了（需要使用注册中心和服务发现机制解决）
+            try(HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+                         .body(bodyBytes)
+                         .execute()) {
+                byte[] result = httpResponse.bodyBytes();
+                //反序列化
+                RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
