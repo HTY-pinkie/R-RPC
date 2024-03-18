4f18b1478339c3e0f08cc82810746830c57c94e2
wb20 <hello@wb20.cn>
2024-03-18 11:01:54 +0800

    first submit

---
+++ example-consumer/src/main/java/com/wb20/example/consumer/ServiceProxy.java
@@ -0,0 +1,11 @@
+package com.wb20.example.consumer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class ServiceProxy implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
