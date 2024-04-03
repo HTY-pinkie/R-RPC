9da3c096cfbe81a8e8b90b36c15ae3983f7b5563
wb20 <hello@wb20.cn>
2024-04-03 18:05:34 +0800

    fifth submit

---
+++ r-rpc-core/src/main/java/com/wb20/rrpc/registry/Registry.java
@@ -0,0 +1,44 @@
+package com.wb20.rrpc.registry;
+
+import com.wb20.rrpc.config.RegistryConfig;
+import com.wb20.rrpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册中心
+ */
+public interface Registry {
+
+    /**
+     * 初始化
+     * @param registryConfig
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务
+     * @param serviceMetaInfo
+     * @throws Exception
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务（服务端）
+     * @param serviceMetaInfo
+     */
+    void unRegister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现（获取某服务的所有节点，消费端）
+     * @param serviceKey 服务健名
+     * @return
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     */
+    void destroy();
+
+}
