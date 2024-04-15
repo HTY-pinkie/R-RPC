908dbd441ea3a5f5e05c80e2f7f44687a53d27b6
wb20 <hello@wb20.cn>
2024-04-15 16:57:38 +0800

    twelve submit

---
+++ r-rpc-core/src/main/java/com/wb20/rrpc/protocol/ProtocolMessageStatusEnum.java
@@ -0,0 +1,29 @@
+package com.wb20.rrpc.protocol;
+
+import lombok.Getter;
+
+/**
+ * 协议消息的状态枚举
+ */
+@Getter
+public enum ProtocolMessageStatusEnum {
+
+    OK("ok", 20),
+    BAD_REQUEST("badRequest", 40),
+    BAD_RESPONSE("badResponse", 50);
+
+    private final String text;
+
+    private final int value;
+
+    ProtocolMessageStatusEnum(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    public static ProtocolMessageStatusEnum getEnumByValue(int value) {
+
+    }
+
+
+}
